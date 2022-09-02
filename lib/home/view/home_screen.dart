bee8d2ea6e11f683681c37ec7134dad6aafaa08c
Akshay <akshayasok1998@gmail.com>
2022-09-02 22:40:07 +0530

    featL setup firebase hosting ci/cd

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,20 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 import 'package:rive/rive.dart';
 
 import '../../core/string_constant.dart';
 import '../../theme/color.dart';
+import '../../widget/button.dart';
 import '../cubit/upload_file_cubit.dart';
+import '../widgets/upload_success.dart';
+import '../widgets/uploading_widget.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => UploadFileCubit(),
+      child: Scaffold(
+        body: Row(
+          children: const [
+            _LeftSideWidget(),
+            _RightSideWidget(),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _RightSideWidget extends StatefulWidget {
+  const _RightSideWidget({super.key});
+
+  @override
+  State<_RightSideWidget> createState() => _RightSideWidgetState();
+}
+
+class _RightSideWidgetState extends State<_RightSideWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<UploadFileCubit, UploadFileState>(
+      builder: (context, state) {
+        return Expanded(
+          child: LayoutBuilder(builder: (context, BoxConstraints constraints) {
+            final width = constraints.maxWidth / 10;
+            return Padding(
+              padding: EdgeInsets.all(width),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    StringConstants.appName,
+                    style: TextStyle(
+                      fontSize: 34,
+                      color: AppColor.primary,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    StringConstants.uploadText,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: AppColor.primary.withOpacity(0.8),
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  if (state.status == Status.success)
+                    UploadSuccessWidget(state: state)
+                  else if (state.status == Status.loading)
+                    UploadingWidget(state: state)
+                  else
+                    AppButton(
+                      title: StringConstants.uploadFile,
+                      onTap: () {
+                        context.read<UploadFileCubit>().pickFile();
+                      },
+                    )
+                ],
+              ),
+            );
+          }),
+        );
+      },
+    );
+  }
+}
+
+class _LeftSideWidget extends StatefulWidget {
+  const _LeftSideWidget();
+  @override
+  State<_LeftSideWidget> createState() => _LeftSideWidgetState();
+}
+
+class _LeftSideWidgetState extends State<_LeftSideWidget> {
   SMIInput? _pressInput;
 
   void _onRiveInit(Artboard artboard) {
@@ -25,148 +104,19 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => UploadFileCubit(),
-      child: Scaffold(
-        body: Row(
-          children: [
-            _LeftSideWidget(
-              onRiveInit: _onRiveInit,
-            ),
-            BlocBuilder<UploadFileCubit, UploadFileState>(
-              builder: (context, state) {
-                if (state.status == Status.loading) {
-                  _pressInput!.value = state.progress!;
-                }
-                return Expanded(
-                  child: LayoutBuilder(builder: (context, BoxConstraints constraints) {
-                    final width = constraints.maxWidth / 10;
-                    return Padding(
-                      padding: EdgeInsets.all(width),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            StringConstants.appName,
-                            style: TextStyle(
-                              fontSize: 34,
-                              color: AppColor.primary,
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            StringConstants.uploadText,
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: AppColor.primary.withOpacity(0.8),
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-                          const SizedBox(height: 32),
-                          if (state.status == Status.success) ...[
-                            const SizedBox(height: 8),
-                            QrImage(
-                              padding: EdgeInsets.zero,
-                              data: state.url!,
-                              version: QrVersions.auto,
-                              size: 200.0,
-                            ),
-                            const SizedBox(height: 8),
-                            SelectableText(
-                              state.url.toString(),
-                              style: const TextStyle(
-                                fontSize: 12,
-                                color: AppColor.green,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            InkWell(
-                              borderRadius: BorderRadius.circular(12),
-                              onTap: () {
-                                context.read<UploadFileCubit>().pickFile();
-                              },
-                              child: Ink(
-                                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 16),
-                                decoration: BoxDecoration(
-                                  color: AppColor.primary,
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
-                                child: const Text(
-                                  StringConstants.uploadNew,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: AppColor.white,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ] else if (state.status == Status.loading)
-                            Ink(
-                              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 16),
-                              decoration: BoxDecoration(
-                                color: AppColor.grey,
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                              child: Text(
-                                ('${state.progress!.toStringAsFixed(1)}% ') + StringConstants.uploaded,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: AppColor.white,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              ),
-                            )
-                          else
-                            InkWell(
-                              borderRadius: BorderRadius.circular(12),
-                              onTap: () {
-                                context.read<UploadFileCubit>().pickFile();
-                              },
-                              child: Ink(
-                                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 16),
-                                decoration: BoxDecoration(
-                                  color: AppColor.primary,
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
-                                child: const Text(
-                                  StringConstants.uploadFile,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: AppColor.white,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                            ),
-                        ],
-                      ),
-                    );
-                  }),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _LeftSideWidget extends StatelessWidget {
-  const _LeftSideWidget({Key? key, this.onRiveInit}) : super(key: key);
-  final void Function(Artboard)? onRiveInit;
-
-  @override
-  Widget build(BuildContext context) {
     return Expanded(
       child: Container(
         color: AppColor.primary,
-        child: RiveAnimation.asset(
-          'assets/rive/tree.riv',
-          onInit: onRiveInit,
+        child: BlocBuilder<UploadFileCubit, UploadFileState>(
+          builder: (context, state) {
+            if (state.status == Status.loading) {
+              _pressInput?.value = state.progress!;
+            }
+            return RiveAnimation.asset(
+              'assets/rive/tree.riv',
+              onInit: _onRiveInit,
+            );
+          },
         ),
       ),
     );
